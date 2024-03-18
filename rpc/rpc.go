5b408dd3181decd87035812c769618e94fa3dcd8
TJ DeVries <devries.timothyj@gmail.com>
2024-03-18 16:50:08 -0400

    step 2: implement basic decode and encode

---
+++ rpc/rpc.go
@@ -0,0 +1,43 @@
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func EncodeMessage(msg any) string {
+	content, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+type BaseMessage struct {
+	Method string `json:"method"`
+}
+
+func DecodeMessage(msg []byte) (string, []byte, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return "", nil, errors.New("Did not find separator")
+	}
+
+	// Content-Length: <number>
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return "", nil, err
+	}
+
+	var baseMessage BaseMessage
+	if err := json.Unmarshal(content[:contentLength], &baseMessage); err != nil {
+		return "", nil, err
+	}
+
+	return baseMessage.Method, content[:contentLength], nil
+}
---
+++ rpc/rpc_test.go
@@ -0,0 +1,36 @@
+package rpc_test
+
+import (
+	"educationalsp/rpc"
+	"testing"
+)
+
+type EncodingExample struct {
+	Testing bool
+}
+
+func TestEncode(t *testing.T) {
+	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodeMessage(EncodingExample{Testing: true})
+	if expected != actual {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Length: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, content, err := rpc.DecodeMessage([]byte(incomingMessage))
+	contentLength := len(content)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLength != 15 {
+		t.Fatalf("Expected: 16, Got: %d", contentLength)
+	}
+
+	if method != "hi" {
+		t.Fatalf("Expected: 'hi', Got: %s", method)
+	}
+}
+
